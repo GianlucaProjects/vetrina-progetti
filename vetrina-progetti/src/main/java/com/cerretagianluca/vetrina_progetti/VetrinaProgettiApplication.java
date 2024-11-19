aeb334edff5def91ad7acf7daa26735e74b94212
Gianluca Cerreta <gianluca2003ba@gmail.com>
2024-11-19 18:29:49 +0100

    creazione progetto

---
+++ vetrina-progetti/src/main/java/com/cerretagianluca/vetrina_progetti/VetrinaProgettiApplication.java
@@ -0,0 +1,13 @@
+package com.cerretagianluca.vetrina_progetti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetrinaProgettiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetrinaProgettiApplication.class, args);
+	}
+
+}
---
+++ vetrina-progetti/src/test/java/com/cerretagianluca/vetrina_progetti/VetrinaProgettiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cerretagianluca.vetrina_progetti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetrinaProgettiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
